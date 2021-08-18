bf42c552b11ce1f5d29102985afc8f63b94fb0ce
Shinya Ishikawa <ishikawa@rt-net.jp>
2021-08-18 17:52:52 +0000

    Refactor

--- firmware/stackchan/robot.ts
+++ firmware/stackchan/robot.ts
@@ -101,21 +101,24 @@ export class Target {
 const staticTarget = new Target(0.1, 0, 0)
 
 export function defaultEyes(): Eye[] {
-  return [{
-    name: 'leftEye',
-    position: {
-      x: 0.03,
-      y: 0.009,
-      z: 0,
-    }
-  }, {
-    name: 'rightEye',
-    position: {
-      x: 0.03,
-      y: -0.009,
-      z: 0,
-    }
-  }]
+  return [
+    {
+      name: 'leftEye',
+      position: {
+        x: 0.03,
+        y: 0.009,
+        z: 0,
+      },
+    },
+    {
+      name: 'rightEye',
+      position: {
+        x: 0.03,
+        y: -0.009,
+        z: 0,
+      },
+    },
+  ]
 }
 
 function getYawPitchFromVector3(vector3: Vector3) {
@@ -157,11 +160,11 @@ function rotateVector3ByYawAndPitch(v: Vector3, yaw: number, pitch: number) {
 }
 
 type Eye = {
-  name: string,
-  position: Vector3,
+  name: string
+  position: Vector3
 }
 
-// type Emotion = 
+// type Emotion =
 //   | 'angry'
 //   | 'disgusted'
 //   | 'fearful'
@@ -175,18 +178,18 @@ type MouthState = {
 }
 
 type EyeState = {
-  name: string,
+  name: string
   gaze: {
-    yaw: number,
-    pitch: number,
+    yaw: number
+    pitch: number
   }
 }
 
 type FaceContext = {
-    eyes: EyeState[]
-    emotion: Emotion
-    mouth: MouthState
-  }
+  eyes: EyeState[]
+  emotion: Emotion
+  mouth: MouthState
+}
 
 type Renderer = {
   render: (faceContext: FaceContext) => unknown
@@ -198,15 +201,15 @@ type Driver = {
 }
 
 function toRadian(deg: number) {
-  return deg * Math.PI / 180
+  return (deg * Math.PI) / 180
 }
 
 function toDegree(rad: number) {
-  return rad * 180 / Math.PI
+  return (rad * 180) / Math.PI
 }
 
 type Saccade = {
-  yaw: number,
+  yaw: number
   pitch: number
 }
 
@@ -220,12 +223,7 @@ export class Robot {
   _saccadeGain: number
   _saccadeInterval: number
   _renderer: Renderer
-  constructor(params: {
-    driver: Driver,
-    renderer: Renderer,
-    eyes: Eye[],
-  }
-  ) {
+  constructor(params: { driver: Driver; renderer: Renderer; eyes: Eye[] }) {
     this._renderer = params.renderer
     this._isMoving = false
     this._pose = {
@@ -239,7 +237,7 @@ export class Robot {
     }
     this._saccade = {
       yaw: 0.0,
-      pitch: 0.0
+      pitch: 0.0,
     }
     this._saccadeGain = 1.0
     this._saccadeInterval = 300
@@ -260,13 +258,14 @@ export class Robot {
     return promise
   }
   async lookAt(target: Vector3) {
+    /* noop */
   }
   saccadeLoop() {
     const gain = this._saccadeGain
     const base = Math.PI / 18
     const saccade = {
       yaw: randomBetween(-base * gain, base * gain),
-      pitch: randomBetween(-base * gain, base * gain)
+      pitch: randomBetween(-base * gain, base * gain),
     }
     this.onSaccade(saccade)
     Timer.set(this.saccadeLoop.bind(this), randomBetween(this._saccadeInterval, this._saccadeInterval * 5))
@@ -276,17 +275,17 @@ export class Robot {
     this.control()
   }
   onPoseChange(pose: Pose) {
-    trace(`onPoseChange__\t`)
+    // trace(`onPoseChange__\t`)
     this._pose = pose
     this.control()
   }
   onTargetChange(targetPosition: Vector3) {
-    trace(`onTargetChange__\t`)
+    // trace(`onTargetChange__\t`)
     this.control()
   }
   follow(target?: Target) {
     if (target == null) {
-      trace('no target specified\n')
+      // trace('no target specified\n')
       return
     }
     this._target = target
@@ -297,10 +296,10 @@ export class Robot {
     this._target = staticTarget
   }
   control() {
-    trace(`control___this._isMoving: ${this._isMoving}\n`)
+    // trace(`control___this._isMoving: ${this._isMoving}\n`)
     // 注視点の計算
     const { yaw, pitch } = this._pose
-    trace(`yaw: ${toDegree(yaw)}, pitch: ${toDegree(pitch)}\n`)
+    // trace(`yaw: ${toDegree(yaw)}, pitch: ${toDegree(pitch)}\n`)
 
     if (this._target == null) {
       return
@@ -308,10 +307,10 @@ export class Robot {
     const v = rotateVector3ByYawAndPitch(this._target, -yaw, -pitch)
     const face: FaceContext = {
       emotion: Emotion.NEUTRAL,
-      eyes: [] as { name: string, gaze: { yaw: number, pitch: number } }[],
+      eyes: [] as { name: string; gaze: { yaw: number; pitch: number } }[],
       mouth: {
-        open: 1.0
-      }
+        open: 1.0,
+      },
     }
     for (const eye of this._eyes) {
       const relative = sub(v, eye.position)
@@ -321,23 +320,23 @@ export class Robot {
         gaze: {
           yaw,
           pitch,
-        }
+        },
       })
     }
     this._renderer.render(face)
     if (!this._isMoving) {
       const { yaw: poseYaw, pitch: posePitch } = getYawPitchFromVector3(v)
-      trace(`target angle: (${toDegree(yaw)}, ${toDegree(pitch)})\n`)
+      // trace(`target angle: (${toDegree(yaw)}, ${toDegree(pitch)})\n`)
       if (poseYaw > Math.PI / 6 || poseYaw < -Math.PI / 6 || posePitch > Math.PI / 6 || posePitch < -Math.PI / 6) {
         const tv = {
           x: this._target.x,
           y: this._target.y,
-          z: this._target.z
+          z: this._target.z,
         }
         const { yaw, pitch } = getYawPitchFromVector3(tv)
         this._isMoving = true
         Timer.set(() => {
-          trace(`this._isMoving: ${this._isMoving} then headding to (${toDegree(yaw)}, ${toDegree(pitch)})\n`)
+          // trace(`this._isMoving: ${this._isMoving} then headding to (${toDegree(yaw)}, ${toDegree(pitch)})\n`)
           this._driver.applyPose({
             yaw,
             pitch,

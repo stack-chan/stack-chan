f5d1918510e47c5f7a73d0955b183322874f64fc
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2022-02-07 14:05:45 +0000

    Fix typings

--- firmware/typings/modules.d.ts
+++ firmware/typings/modules.d.ts
@@ -1,8 +1,8 @@
 declare module "modules" {
   export default class Modules {
-    static get host(): any;
-    static get archive(): any;
     static has(name: string): boolean;
     static importNow(name: string): any;
+    static get host(): string[];
+    static get archive(): string[];
   }
 }

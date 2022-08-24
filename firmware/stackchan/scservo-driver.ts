bc383c5259200ef11dc358fc7b41a26df8a70ffd
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2022-08-24 15:56:25 +0900

    Refactor

--- firmware/stackchan/scservo-driver.ts
+++ firmware/stackchan/scservo-driver.ts
@@ -24,21 +24,27 @@ export class SCServoDriver {
     const panAngle = 90 - (pose.yaw * 180) / Math.PI
     const tiltAngle = 90 - Math.min(Math.max((pose.pitch * 180) / Math.PI, -25), 10)
     trace(`applying (${pose.yaw}, ${pose.pitch}) => (${panAngle}, ${tiltAngle})\n`)
-    await this._pan.setAngleInTime(panAngle, time * 1000)
-    await this._tilt.setAngleInTime(tiltAngle, time * 1000)
+    await Promise.all([
+      this._pan.setAngleInTime(panAngle, time * 1000),
+      this._tilt.setAngleInTime(tiltAngle, time * 1000),
+    ])
     Timer.set(async () => {
-      await this._pan.setTorque(false)
-      await this._tilt.setTorque(false)
+      await Promise.all([this._pan.setTorque(false), this._tilt.setTorque(false)])
     }, time * 1000 + 10)
   }
   async poseLoop(): Promise<void> {
     if (this._onPoseChanged == null) {
       return
     }
-    let status = await this._pan.readStatus()
-    const yaw = (-Math.PI * (status.angle - 90)) / 180
-    status = await this._tilt.readStatus()
-    const pitch = (Math.PI * (status.angle - 90)) / 180
+    const [p1, p2] = await Promise.allSettled([this._pan.readStatus(), this._tilt.readStatus()])
+    if (p1.status != 'fulfilled' || p2.status != 'fulfilled') {
+      return
+    }
+    if (p1.value == null || p2.value == null) {
+      return
+    }
+    const yaw = (-Math.PI * (p1.value.angle - 90)) / 180
+    const pitch = (Math.PI * (p2.value.angle - 90)) / 180
     this._onPoseChanged({
       yaw,
       pitch,

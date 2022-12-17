e9638a82e1ce819c2f735c3d7a6b7599b72010b8
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2022-12-18 00:28:51 +0900

    WIP: add speech streaming function

---
+++ firmware/stackchan/speech/tts-local.ts
@@ -0,0 +1,75 @@
+/* eslint-disable prefer-const */
+import AudioOut from 'pins/audioout'
+import ResourceStreamer from 'resourcestreamer'
+import calculatePower from 'calculate-power'
+import speechData from 'speeches'
+
+const speeches = speechData.speeches
+const speechDic = new Map()
+for (const [key, text] of Object.entries(speeches)) {
+  speechDic.set(text, key)
+}
+/* global trace, SharedArrayBuffer */
+
+type TTSProperty = {
+  onPlayed: (number) => void
+}
+
+class TTS {
+  static streamer: ResourceStreamer
+  static audio: AudioOut
+  onPlayed: (number) => void
+  constructor(props: TTSProperty) {
+    this.onPlayed = props.onPlayed
+  }
+  say(text): Promise<void> {
+    const key = speechDic.get(text)
+    if (key == null) {
+      throw new Error(`No speech for "${text}"`)
+    }
+    return this.stream(key)
+  }
+  async stream(key: string): Promise<void> {
+    if (TTS.audio == null) {
+      TTS.audio = new AudioOut({ streams: 1 })
+    }
+    return new Promise((resolve, reject) => {
+      if (TTS.streamer != null) {
+        reject(new Error('already playing'))
+        return
+      }
+      TTS.streamer = new ResourceStreamer({
+        path: `${key}.maud`,
+        audio: {
+          out: TTS.audio,
+          stream: 0,
+          sampleRate: 11025,
+        },
+        onPlayed(buffer) {
+          const power = calculatePower(buffer)
+          this.onPlayed(power)
+        },
+        onReady(state) {
+          trace(`Ready: ${state}\n`)
+          if (state) {
+            TTS.audio.start()
+          } else {
+            TTS.audio.stop()
+          }
+        },
+        onError(e) {
+          trace('ERROR: ', e, '\n')
+          TTS.streamer = null
+          reject(new Error('unknown error occured'))
+        },
+        onDone() {
+          trace('DONE\n')
+          TTS.streamer.close()
+          TTS.streamer = null
+          resolve()
+        },
+      })
+    })
+  }
+}
+export default TTS

cd4fde261278206af03838e04a7ae42c325e9778
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2023-01-05 01:15:40 +0900

    Simplify mod

--- firmware/mods/monologue/mod.js
+++ firmware/mods/monologue/mod.js
@@ -1,30 +1,22 @@
-import Timer from 'timer'
 import { speeches } from 'speeches_monologue'
 import { randomBetween } from 'stackchan-util'
 
-let robot
 const keys = speeches.keys()
 
-async function sayMonologue() {
+async function sayMonologue(robot) {
   const idx = Math.floor(randomBetween(0, keys.length))
   const key = keys[idx]
   await robot.say(key)
-  const next = randomBetween(60, 30 * 60) * 1000 // ms
-  // Timer.set(sayMonologue, next)
 }
 
-function onRobotCreated(r) {
-  robot = r
-  // sayMonologue()
-}
-
-async function onButtonPressed(button, pressed) {
-  if (button === 'A' && pressed) {
-    await sayMonologue()
+function onRobotCreated(robot) {
+  robot.button.a.onChanged = function () {
+    if (this.read()) {
+      sayMonologue(robot)
+    }
   }
 }
 
 export default {
   onRobotCreated,
-  onButtonPressed
 }

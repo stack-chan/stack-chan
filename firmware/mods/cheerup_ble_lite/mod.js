6701a2818581595933a384237c2f435eb80eddf2
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2023-01-08 00:06:16 +0900

    Refine cheerup_ble_lite

--- firmware/mods/cheerup_ble_lite/mod.js
+++ firmware/mods/cheerup_ble_lite/mod.js
@@ -1,101 +1,62 @@
-/* global screen */
-
-import { AvatarMouth } from 'avatar'
 import StkServer from 'stk-server'
 import Timer from 'timer'
-import ttsResources from 'speeches'
+import { speeches } from 'speeches_cheerup'
+import { randomBetween } from 'stackchan-util'
 
-let stkServer
 let pose
-let ap
-let mouth
-let robot
 let hooray = false
 let saying = false
 let connected = false
-let panAngle = 90
-let tiltAngle = 90
-const speeches = ttsResources.speeches
-const texts = Object.values(speeches)
+let yaw = 0
+let pitch = 0
+const keys = Object.keys(speeches)
 
-const AvatarMouthSkinTexture = Texture.template({
-  path: 'mouth_smile-alpha.bmp',
-})
-
-const AvatarMouthSkin = Skin.template({
-  Texture: AvatarMouthSkinTexture,
-  width: 80,
-  height: 40,
-  variants: 80,
-  states: 40,
-  color: 'white',
-})
-
-async function sayHooray() {
+async function sayHooray(robot) {
   if (saying) {
     return
   }
   saying = true
-  const text = texts[Math.floor(Math.random() * texts.length)]
-  const avatar = ap.content('avatar')
-  avatar.delegate('startSpeech')
-  await robot?.speak(text).catch(() => {
-    trace('something wrong\n')
-  })
-  avatar.delegate('stopSpeech')
+  const key = keys[Math.floor(randomBetween(0, keys.length))]
+  await robot.say(key)
   saying = false
 }
 
-function onApplicationCreated(newApp) {
-  mouth = new AvatarMouth({
-    left: 120,
-    top: 128,
-    name: 'mouth',
-    color: 'white',
-    skin: new AvatarMouthSkin(),
-  })
-  ap = newApp
-  const avatar = ap.content('avatar')
-  avatar.replace(avatar.content('mouth'), mouth)
-}
-
-function onRobotCreated(newRobot) {
-  robot = newRobot
-  stkServer = new StkServer({
+function onRobotCreated(robot) {
+  new StkServer({
     onConnected: () => {
       trace('connected\n')
+      robot.setTorque(true)
       connected = true
     },
     onReceive: (newPose) => {
       pose = newPose
     },
     onDisconnected: () => {
+      robot.setTorque(false)
       connected = false
-      robot?._driver._pan.setTorque(false)
-      robot?._driver._pan.setTorque(false)
     },
   })
   Timer.repeat(async () => {
     if (pose == null || !connected) {
       return
     }
-    trace(`applying: ${pose}\n`)
-    const newPanAngle = 90 - (pose.yaw * 180) / Math.PI
-    const newTiltAngle = 90 - Math.min(Math.max((pose.pitch * 180) / Math.PI, -25), 10)
-    panAngle = 0.8 * panAngle + 0.2 * newPanAngle
-    tiltAngle = 0.8 * tiltAngle + 0.2 * newTiltAngle
-    await robot?._driver._pan.setAngle(panAngle)
-    await robot?._driver._tilt.setAngle(tiltAngle)
 
-    mouth.state = pose.emotion === 'HAPPY' ? 1 : 0
+    // simple low pass filter
+    yaw = yaw * 0.5 + pose.yaw * 0.5
+    pitch = pitch * 0.5 + pose.pitch * 0.5
+    robot.setPose(yaw, pitch, 0)
+
+    // emotion
+    robot.setEmotion(pose.emotion)
+
+    // hooray on rising edge
     if (!hooray && pose.hooray) {
-      sayHooray()
+      sayHooray(robot)
     }
     hooray = pose.hooray
-  }, 33)
+  }, 100)
 }
 
 export default {
-  onRobotCreated,
-  onApplicationCreated,
+  onRobotCreated
 }
--- firmware/mods/cheerup_ble_lite/speeches_cheerup.js
+++ firmware/mods/cheerup_ble_lite/speeches_cheerup.js
@@ -1,11 +1,7 @@
-const speeches = {
+export const speeches = Object.freeze({
   '01-yay': 'いぇーい',
   '02-hooray': 'わーい',
   '03-wow': 'おー',
   '04-wonderful': 'すごーい',
   '05-congrats': 'おめでとう',
-}
-
-export default {
-  speeches,
-}
+})

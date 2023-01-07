05f8361d1a2e109581f0f92c57e230e4dc7e527b
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2023-01-08 00:35:13 +0900

    Fix calibration
    Still under construction

--- firmware/mods/calibration/mod.js
+++ firmware/mods/calibration/mod.js
@@ -1,57 +1,55 @@
 import Timer from 'timer'
-let robot
-let pan
-let tilt
-let active = false
-async function onRobotCreated(r) {
-  robot = r
-  pan = r._driver._pan
-  tilt = r._driver._tilt
+
+/**
+ * @brief calibrates SCServo offset angle
+ * @note This mod is under construction. setting pan/tilt offset does not work properly.
+ * @param {Robot} robot 
+ */
+async function onRobotCreated(robot) {
+  let pan = robot._driver._pan
+  let tilt = robot._driver._tilt
+  robot.button.a.onChanged = async function () {
+    if (!this.read()) {
+      return
+    }
+    trace('setting pan offset\n')
+    const { angle: panAngle } = await pan.readStatus()
+    await pan.setOffsetAngle(panAngle - 90)
+    await pan.saveSettings()
+  }
+  robot.button.b.onChanged = async function () {
+    if (!this.read()) {
+      return
+    }
+    trace('setting tilt offset\n')
+    const { angle: tiltAngle } = await tilt.readStatus()
+    await tilt.setOffsetAngle(tiltAngle - 90)
+    await tilt.saveSettings()
+  }
+  robot.button.c.onChanged = async function () {
+    if (!this.read()) {
+      return
+    }
+    trace('setting zero\n')
+    await tilt.setAngle(100)
+    await pan.setAngle(100)
+    Timer.set(async () => {
+      await tilt.setTorque(false)
+      await pan.setTorque(false)
+    }, 500)
+  }
+
   await pan.setOffsetAngle(0)
   await tilt.setOffsetAngle(0)
   Timer.repeat(async () => {
-    active = true
     const { angle: panAngle } = await pan.readStatus()
     const { angle: tiltAngle } = await tilt.readStatus()
     const panOffset = await pan.readOffsetAngle()
     const tiltOffset = await tilt.readOffsetAngle()
     trace(`(pan, tilt) => (${panAngle}, ${tiltAngle}), (panOffset, tiltOffset) => (${panOffset}, ${tiltOffset})\n`)
-    active = false
   }, 1000)
-}
-
-async function onButtonChange(button, pressed) {
-  if (!pressed) {
-    return
-  }
-  switch (button) {
-    case 'A':
-      trace('setting pan offset\n')
-      const { angle: panAngle } = await pan.readStatus()
-      active = true
-      await pan.setOffsetAngle(panAngle - 90)
-      await pan.saveSettings()
-      break
-    case 'B':
-      trace('setting tilt offset\n')
-      const { angle: tiltAngle } = await tilt.readStatus()
-      await tilt.setOffsetAngle(tiltAngle - 90)
-      await tilt.saveSettings()
-      break
-    case 'C':
-      trace('setting zero\n')
-      await tilt.setAngle(100)
-      await pan.setAngle(100)
-      Timer.set(async () => {
-        await tilt.setTorque(false)
-        await pan.setTorque(false)
-      }, 500)
-      break
-  }
 }
 
 export default {
   onRobotCreated,
-  onButtonChange,
-  autoLoop: false,
 }

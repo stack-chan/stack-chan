00e544b237fe05f4e0400c0e60fdeeb45fdc765f
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2023-01-25 01:26:25 +0900

    Remove comments

--- firmware/stackchan/drivers/rs30x-driver.ts
+++ firmware/stackchan/drivers/rs30x-driver.ts
@@ -19,7 +19,6 @@ export class RS30XDriver {
   async setTorque(torque: boolean): Promise<void> {
     await this._pan.setTorque(torque)
     await this._tilt.setTorque(torque)
-    // await Promise.all([this._pan.setTorque(torque), this._tilt.setTorque(torque)])
   }
 
   async applyRotation(ori: Rotation, time = 0.5): Promise<void> {
@@ -29,11 +28,9 @@ export class RS30XDriver {
     if (time === 0) {
       await this._pan.setAngle(panAngle)
       await this._tilt.setAngle(tiltAngle)
-      // await Promise.all([this._pan.setAngle(panAngle), this._tilt.setAngle(tiltAngle)])
     } else {
       await this._pan.setAngleInTime(panAngle, time)
       await this._tilt.setAngleInTime(tiltAngle, time)
-      // await Promise.all([this._pan.setAngleInTime(panAngle, time), this._tilt.setAngleInTime(tiltAngle, time)])
     }
   }
   async getRotation(): Promise<Maybe<Rotation>> {
@@ -41,16 +38,6 @@ export class RS30XDriver {
     const tiltAngle = await this._tilt.readStatus().catch((): null => null)
     const y = (-Math.PI * yawAngle) / 180
     const p = (-Math.PI * tiltAngle) / 180
-    // const [p1, p2] = await Promise.allSettled([this._pan.readStatus(), this._tilt.readStatus()])
-    // if (p1.status != 'fulfilled' || p2.status != 'fulfilled') {
-    //   return {
-    //     success: false,
-    //     reason: 'response corrupted',
-    //   }
-    // }
-    // const y = (-Math.PI * p1.value) / 180
-    // const p = (-Math.PI * p2.value) / 180
-    // trace(`got rotation: (${p1.value}, ${p2.value}) => (${y}, ${p}) => \n`)
     return {
       success: true,
       value: {

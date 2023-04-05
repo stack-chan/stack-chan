dadbd95fb070d2e3a298a1b58a40afbc74067b21
STC <stc1988@users.noreply.github.com>
2023-04-06 00:27:06 +0900

    monologue compatible with tts.type remote

--- firmware/mods/monologue/mod.js
+++ firmware/mods/monologue/mod.js
@@ -1,12 +1,13 @@
 import { speeches } from 'speeches_monologue'
 import { randomBetween } from 'stackchan-util'
+import config from 'mc/config'
 
 const keys = Object.keys(speeches)
 
 async function sayMonologue(robot) {
   const idx = Math.floor(randomBetween(0, keys.length))
   const key = keys[idx]
-  await robot.say(key)
+  await robot.say(config.tts.type=='local'? key:speeches[key])
 }
 
 function onRobotCreated(robot) {

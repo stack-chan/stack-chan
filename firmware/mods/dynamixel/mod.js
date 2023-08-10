924c7f57d763de0a68edc3123385a90924375245
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2023-08-11 01:21:15 +0900

    WIP

--- firmware/mods/dynamixel/mod.js
+++ firmware/mods/dynamixel/mod.js
@@ -1,35 +1,48 @@
-import Dynamixel from 'dynamixel'
+import Dynamixel, { OPERATING_MODE } from 'dynamixel'
 import Timer from 'timer'
-export function onLaunch() {
+
+export async function onLaunch() {
+  const INTERVAL = 50
+  const HZ = 1000 / INTERVAL
   // const servo = new Dynamixel({
   //   id: 1,
   // })
-  
+
   let flag = true
+  let toruqeEnable = true
   let id = 1
-  let baudrates = [
-    9600,
-    57600,
-    115200,
-    1000000
-  ]
+  let baudrates = [9600, 57600, 115200, 1000000]
   let idx = 0
+  let count = 0
   const servo = new Dynamixel({
     id,
   })
-  Timer.repeat(() => {
-    trace(`sending to ${id}\n`)
-    servo.setId(id)
-    servo.setLED(flag);
+  trace('operating mode\n')
+  // await servo.setOperatingMode(OPERATING_MODE.CURRENT_BASE_POSITION)
+  await servo.setOperatingMode(OPERATING_MODE.CURRENT)
+
+  trace('enable torque\n')
+  await servo.setTorque(toruqeEnable)
+  // await servo.setGoalCurrent(128)
+  let pos = 0
+  let current = 0
+  Timer.repeat(async () => {
+    await servo.setLED(flag)
+    count = (count + 1) % (2 * HZ)
+    // count += 1
+    pos = 2048 + Math.sin(2 * Math.PI * count / HZ) * 512
+    current = Math.floor(Math.sin(Math.PI * count / HZ) * 64)
+    await servo.setGoalCurrent(current)
+    // await servo.setGoalPosition(pos);
+    // const value = servo.readPresentCurrent();
+    // if (count % 10 === 0) {
+    //   toruqeEnable = !toruqeEnable
+    //   servo.setTorque(toruqeEnable)
+    // }
     // servo.setGoalPosition(999);
-    if (id === 255) {
-      Timer.delay(500)
-      idx = (idx + 1) % baudrates.length
-      trace(`setting baudrates to ${baudrates[idx]}`)
-      Dynamixel.setBaud(baudrates[idx])
-    }
     // id = (id + 1) & 0xff
     flag = !flag
-  }, 500)
+  }, INTERVAL)
+  // }, 1000)
   return false
 }

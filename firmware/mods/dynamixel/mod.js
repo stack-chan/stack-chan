5c7e581acbc2cc0bbbbb14f61f3d786179dc6c46
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2023-08-14 11:59:11 +0900

    random move using current-based position ctrl

--- firmware/mods/dynamixel/mod.js
+++ firmware/mods/dynamixel/mod.js
@@ -1,48 +1,45 @@
 import Dynamixel, { OPERATING_MODE } from 'dynamixel'
+import { randomBetween } from 'stackchan-util'
 import Timer from 'timer'
 
 export async function onLaunch() {
   const INTERVAL = 50
   const HZ = 1000 / INTERVAL
-  // const servo = new Dynamixel({
-  //   id: 1,
-  // })
-
   let flag = true
   let toruqeEnable = true
   let id = 1
-  let baudrates = [9600, 57600, 115200, 1000000]
-  let idx = 0
   let count = 0
   const servo = new Dynamixel({
     id,
   })
   trace('operating mode\n')
-  // await servo.setOperatingMode(OPERATING_MODE.CURRENT_BASE_POSITION)
-  await servo.setOperatingMode(OPERATING_MODE.CURRENT)
+  await servo.setOperatingMode(OPERATING_MODE.CURRENT_BASED_POSITION)
 
   trace('enable torque\n')
   await servo.setTorque(toruqeEnable)
-  // await servo.setGoalCurrent(128)
-  let pos = 0
-  let current = 0
+  let goalPosition = 0
+  const P_GAIN = 0.1
+  await servo.setGoalPosition(0)
   Timer.repeat(async () => {
     await servo.setLED(flag)
     count = (count + 1) % (2 * HZ)
-    // count += 1
-    pos = 2048 + Math.sin(2 * Math.PI * count / HZ) * 512
-    current = Math.floor(Math.sin(Math.PI * count / HZ) * 64)
+    if (count === 0) {
+      goalPosition = Math.floor(randomBetween(0, 500))
+      trace(`goal position changed: ${goalPosition}`)
+      await servo.setGoalPosition(goalPosition)
+    }
+    const result = await servo.readPresentPosition()
+    if (!result.success) {
+      trace(`failed: ${ result.reason }\n`)
+      flag = !flag
+      return
+    }
+    const position = result.value.position
+    trace(`pos: ${ position } \n`)
+    const current = Math.abs(Math.floor(Math.max(Math.min((goalPosition - position) * P_GAIN, 128), -128)))
+    trace(`current: ${current}\n`)
     await servo.setGoalCurrent(current)
-    // await servo.setGoalPosition(pos);
-    // const value = servo.readPresentCurrent();
-    // if (count % 10 === 0) {
-    //   toruqeEnable = !toruqeEnable
-    //   servo.setTorque(toruqeEnable)
-    // }
-    // servo.setGoalPosition(999);
-    // id = (id + 1) & 0xff
     flag = !flag
   }, INTERVAL)
-  // }, 1000)
   return false
 }

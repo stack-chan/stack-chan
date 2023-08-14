3dfb50d7c06337fbebcd1acfa848f25e09d496b4
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2023-08-14 15:03:52 +0900

    Double servos

--- firmware/mods/dynamixel/mod.js
+++ firmware/mods/dynamixel/mod.js
@@ -3,43 +3,63 @@ import { randomBetween } from 'stackchan-util'
 import Timer from 'timer'
 
 export async function onLaunch() {
-  const INTERVAL = 50
+  const INTERVAL = 100
   const HZ = 1000 / INTERVAL
   let flag = true
   let toruqeEnable = true
-  let id = 1
   let count = 0
   const servo = new Dynamixel({
-    id,
+    id: 1
+  })
+  const servo2 = new Dynamixel({
+    id: 2
   })
   trace('operating mode\n')
   await servo.setOperatingMode(OPERATING_MODE.CURRENT_BASED_POSITION)
+  await servo2.setOperatingMode(OPERATING_MODE.CURRENT_BASED_POSITION)
 
   trace('enable torque\n')
   await servo.setTorque(toruqeEnable)
-  let goalPosition = 0
+  await servo2.setTorque(toruqeEnable)
+  let pos = [0, 0]
   const P_GAIN = 0.1
+  const P_GAIN2 = 0.3
   await servo.setGoalPosition(0)
+  await servo2.setGoalPosition(0)
   Timer.repeat(async () => {
     await servo.setLED(flag)
+    await servo2.setLED(!flag)
+    flag = !flag
+
     count = (count + 1) % (2 * HZ)
     if (count === 0) {
-      goalPosition = Math.floor(randomBetween(0, 500))
-      trace(`goal position changed: ${goalPosition}`)
-      await servo.setGoalPosition(goalPosition)
+      pos[0] = Math.floor(randomBetween(-512, 512))
+      pos[1] = Math.floor(randomBetween(0, 256))
+      trace(`goal position changed: (${pos[0]}, ${pos[1]})\n`)
+      await servo.setGoalPosition(pos[0])
+      await servo2.setGoalPosition(pos[1])
     }
     const result = await servo.readPresentPosition()
     if (!result.success) {
       trace(`failed: ${ result.reason }\n`)
-      flag = !flag
       return
     }
     const position = result.value.position
-    trace(`pos: ${ position } \n`)
-    const current = Math.abs(Math.floor(Math.max(Math.min((goalPosition - position) * P_GAIN, 128), -128)))
-    trace(`current: ${current}\n`)
+    // trace(`pos: ${ position } \n`)
+    const current = Math.min(Math.abs(pos[0] - position) * P_GAIN, 80)
+    // trace(`current: ${current}\n`)
     await servo.setGoalCurrent(current)
-    flag = !flag
+
+    const result2 = await servo2.readPresentPosition()
+    if (!result2.success) {
+      trace(`2 failed: ${ result2.reason }\n`)
+      return
+    }
+    const position2 = result2.value.position
+    // trace(`pos2: ${ position2 } \n`)
+    const current2 = Math.min(Math.abs(pos[1] - position2) * P_GAIN2, 100)
+    // trace(`current2: ${current2}\n`)
+    await servo2.setGoalCurrent(current2)
   }, INTERVAL)
   return false
 }

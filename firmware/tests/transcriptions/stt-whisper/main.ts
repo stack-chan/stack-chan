72faa2e70c1c858ddc78b0cd7b5ec9023d459707
STC <stc1988@users.noreply.github.com>
2024-10-13 01:01:08 +0900

    add stt-whisper

---
+++ firmware/tests/transcriptions/stt-whisper/main.ts
@@ -0,0 +1,23 @@
+import config from 'mc/config'
+import STT from 'stt-whisper'
+import Resource from 'Resource'
+
+const token = config.token
+
+const audio = new Resource('speak.wav')
+if (!token || token === 'YOUR_API_KEY_HERE') throw new Error('API token is missing.')
+
+const stt = new STT({
+  apiKey: token,
+})
+
+try {
+  const result = await stt.transcription(audio)
+  if (result.success === true) {
+    trace(result.value)
+  } else {
+    trace(`Error: ${result.reason}`)
+  }
+} catch (error) {
+  trace(`An error occurred: ${error.message}`)
+}

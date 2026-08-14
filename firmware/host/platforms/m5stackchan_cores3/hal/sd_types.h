ad303cde97311071dd4ddf53dc7e05c69f3f98ec
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2026-08-15 00:19:51 +0900

    fix(firmware): support ESP-IDF SD header move

--- firmware/host/platforms/m5stackchan_cores3/hal/sd_types.h
+++ firmware/host/platforms/m5stackchan_cores3/hal/sd_types.h
@@ -1,2 +1,6 @@
-// Moddable exposes only one C_INCLUDES root; forward ESP-IDF component headers through it.
+#pragma once
+#if __has_include("esp_hal_sd/include/hal/sd_types.h")
+#include "esp_hal_sd/include/hal/sd_types.h"
+#else
 #include "hal/include/hal/sd_types.h"
+#endif
